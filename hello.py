30a1a3f927e206676923bec2b32ed4dd8be0567b
DineshMadav <DineshMadav@users.noreply.github.com>
2020-09-09 11:14:24 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -9,20 +9,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Index!"
-
-@app.route("/hello")
-def hello():
-    return "Hello World!"
-
-@app.route("/members")
-def members():
-    return "Members"
-
-@app.route("/members/<string:name>/")
-def getMember(name):
-    return name
-
+    user = {'username': 'Miguel'}
+    return render_template('index.html', title='Home', user=user)
 
 @app.route('/user/<username>')
 def show_user_profile(username):
